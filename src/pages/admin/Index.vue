7afd6c19687ae650200b7a3aab735fdcd1a169b4
Ricardo Gauto <ricardogauto11@gmail.com>
2018-10-25 19:53:48 -0300

    Modulo Admin

---
+++ src/pages/admin/Index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="admin-new">
+      <div class="container">
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <ul class="admin-menu">
+            <li>
+              <router-link to="/admin">View Products</router-link>
+            </li>
+            <li>
+              <router-link to="/admin/new">New Product</router-link>
+            </li>
+          </ul>
+        </div> <!-- col-lg-3 col-md-3 col-sm-12 col-xs-12 -->
+        <router-view></router-view>
+      </div> <!-- container -->
+    </div> <!-- admin-new -->
+  </div>
+</template>
+<script>
+export default {
+  name: 'home',
+  data() {
+    return {
+      msg: 'Welcome to the Admin Page',
+    };
+  },
+};
+</script>
+
---
+++ src/pages/admin/Products.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <div class="title">
+      <h1>this is Admin</h1>
+    </div>
+  </div>
+</template>
